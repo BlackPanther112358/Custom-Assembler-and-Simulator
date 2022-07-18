382e9d3c15ff6cdb9953b38f9c8508ff2da06b1f
Abhay Chowdhry <abhay21508@iiitd.ac.in>
2022-07-18 23:09:28 +0530

    Iteration 1, Type A, B, C

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -12,77 +12,116 @@ D={"ld":"10100", "st":"10101"}
 E={"jmp":"11111", "jlt":"01100", "jgt":"01101", "je":"01111"}
 F={"hlt":"01010"}
 
-flags = {"overflow": False}
+flags = {"overflow": False, "Less than Flag": False, "Greater than flag": False,"Equal to flag": False}
 
+num = 0
+val = 0
 
+def reset():
+    register[7] = 0
+    
 def typeA(op:str, r1:str, r2:str, r3:str):
 
-    val3 = bin(int(r1))[2:]
-    val2 = bin(int(r2))[2:]
-    val1 = bin(int(r3))[2:]
+    val3 = int(r1, 2)
+    val2 = int(r2, 2)
+    val1 = int(r3, 2)
 
     #Add
     if(op == "10000"):
+        register[7] = 0
         register[val3] = register[val2] + register[val1]
 
-        if(len(str(int(val3,2))) > 3):
+        if(register[val3] >= 65536):
             flags["overflow"] = True
-        return
-
+            register[val3] %= 65536
+        
     #Subtract
     elif(op == "10001"):
+        register[7] = 0
         if val1 >= val2:
-            register[val3] = register[val2] + register[val1]
+            register[val3] = register[val2] - register[val1]
         else:
             register[val3] = 0  
             flags["overflow"] = True
-        return
-
+        
     #Multiply
-    if(op == "10110"):
+    elif(op == "10110"):
+        register[7] = 0
         register[val3] = register[val2] * register[val1]
 
-        if(len(str(int(val3,2))) > 3):
+        if(register[val3] >= 65536):
             flags["overflow"] = True
-        return
 
     #XOR
-    if(op == "11010"):
+    elif(op == "11010"):
+        register[7] = 0
         register[val3] = register[val2] ^ register[val1]
-        return
 
     #OR
-    if(op == "11011"):
+    elif(op == "11011"):
+        register[7] = 0
         register[val3] = register[val2] | register[val1]
-        return
 
     #AND
-    if(op == "11100"):
+    elif(op == "11100"):
+        register[7] = 0
         register[val3] = register[val2] & register[val1]
-        return
   
 def typeB(op:str, r1:str, imm:str):
     
-    val1 = bin(int(r1))[2:]
-    imm = bin(int(imm))[2:]
+    val1 = int(r1, 2)
+    imm = int(imm,  2)
 
     #Mov
     if(op == "10010"):
+        register[7] = 0
         register[val1] = imm
-        return
 
     #LeftShift
-    if(op == "11001"):
+    elif(op == "11001"):
+        register[7] = 0
         register[val1] = val1 << imm
-        return
 
     #RightShift
-    if(op == "11000"):
+    elif(op == "11000"):
+        register[7] = 0
         register[val1] = val1 >> imm
-        return
 
 def typeC(op:str, r1:int, r2:int):
-    return
+
+    val1 = int(r1, 2)
+    val2 = int(r2, 2)
+
+    #Mov Reg
+    if(op == "10011"):
+        register[7] = 0
+        register[val1] = register[val2]
+
+    #Divide
+    elif(op == "10111"):
+        register[7] = 0
+        register[0] = val1//val2
+        register[1] = val1%val2
+
+    #Invert
+    elif(op == "11101"):
+        temp = val2
+        for i in range(16):
+            val2 ^= (1 << i)
+        
+        val1 = val2
+        val2 = temp      
+
+    #Compare
+    elif(op == "11110"):
+        if(val1 > val2):
+            num |= (1 << 1)
+        
+        elif(val1 < val2):
+            num |= (1 << 2)
+        
+        elif(val1 == val2):
+            num |= (1 << 0) 
 
 def typeD(op:str, r1:int, mem_addr:int):
     return
