ee79d44776b8078d4be5b328f967418022f7eb8a
Abhay Chowdhry <abhay21508@iiitd.ac.in>
2022-07-18 22:02:24 +0530

    Updated type A and B

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -12,11 +12,74 @@ D={"ld":"10100", "st":"10101"}
 E={"jmp":"11111", "jlt":"01100", "jgt":"01101", "je":"01111"}
 F={"hlt":"01010"}
 
-def typeA(op:str, r1:int, r2:int, r3:int):
-    return
+flags = {"overflow": False}
 
-def typeB(op:str, r1:int, imm:int):
-    return
+
+def typeA(op:str, r1:str, r2:str, r3:str):
+
+    val3 = bin(int(r1))[2:]
+    val2 = bin(int(r2))[2:]
+    val1 = bin(int(r3))[2:]
+
+    #Add
+    if(op == "10000"):
+        register[val3] = register[val2] + register[val1]
+
+        if(len(str(int(val3,2))) > 3):
+            flags["overflow"] = True
+        return
+
+    #Subtract
+    elif(op == "10001"):
+        if val1 >= val2:
+            register[val3] = register[val2] + register[val1]
+        else:
+            register[val3] = 0  
+            flags["overflow"] = True
+        return
+
+    #Multiply
+    if(op == "10110"):
+        register[val3] = register[val2] * register[val1]
+
+        if(len(str(int(val3,2))) > 3):
+            flags["overflow"] = True
+        return
+
+    #XOR
+    if(op == "11010"):
+        register[val3] = register[val2] ^ register[val1]
+        return
+
+    #OR
+    if(op == "11011"):
+        register[val3] = register[val2] | register[val1]
+        return
+
+    #AND
+    if(op == "11100"):
+        register[val3] = register[val2] & register[val1]
+        return
+  
+def typeB(op:str, r1:str, imm:str):
+    
+    val1 = bin(int(r1))[2:]
+    imm = bin(int(imm))[2:]
+
+    #Mov
+    if(op == "10010"):
+        register[val1] = imm
+        return
+
+    #LeftShift
+    if(op == "11001"):
+        register[val1] = val1 << imm
+        return
+
+    #RightShift
+    if(op == "11000"):
+        register[val1] = val1 >> imm
+        return
 
 def typeC(op:str, r1:int, r2:int):
     return
