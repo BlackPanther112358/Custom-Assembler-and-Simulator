b1476c53d8782772f5dea94550f719bfd7e113b8
Aakarsh Jain <jainaakarsh09@gmail.com>
2022-08-06 16:50:35 +0530

    Fixed a lot of bugs

--- SimpleSimulator/main.py
+++ SimpleSimulator/main.py
@@ -1,76 +1,30 @@
 import math
 import matplotlib.pyplot as plt
+# import sys
 
 mem = [0 for i in range(256)]
 opcodes = {}
 types = {}
 register = [0 for i in range(8)]
+new_pc = -1
 pc = -1
 halted = False
 trace = []
-# reverse these
-# A={"add":"10000", "sub":"10001", "mul":"10110", "xor":"11010","or":"11011", "and":"11100", "addf":"00000", "subf":"00001"}
-# B={'10010': 'mov', '11000': 'rs', '11001': 'ls', '00010': 'movf'}
-# C={'10011': 'mov', '10111': 'div', '11101': 'not', '11110': 'cmp'}
-# D={"ld":"10100", "st":"10101"}
-# E={"jmp":"11111", "jlt":"01100", "jgt":"01101", "je":"01111"}
-# F={"hlt":"01010"}
 
-dictionary ={'00000':'A','00001':'A','00010':'B','00011':'C','00100':'D','00101':'D','00110':'A','00111':'C','01000':'B','01001':'B','01010':'A','01011':'A','01100':'A','01101':'C','01110':'C','01111':'E','10000':'E','10001':'E','10010':'E','10011':'F'}
-      
-flags = {"overflow": False, "Less than Flag": False, "Greater than flag": False,"Equal to flag": False}
+
+dictionary = {'10000': 'A', '10001': 'A', '10110': 'A', '11010': 'A', '11011': 'A','11100': 'A','00000': 'A', '00001': 'A','10010': 'B','11000': 'B','11001': 'B', '00010': 'B',  '10011': 'C', '10111': 'C', '11101': 'C', '11110': 'C', '10100': 'D', '10101': 'D','11111': 'E', '01100': 'E', '01101': 'E', '01111': 'E', '01010': 'F'}
+
+# flags = {"overflow": False, "Less than Flag": False, "Greater than flag": False,"Equal to flag": False}
+
+# FV(7) , FL(8) , FG(9) , FE(10) is flag register for overflow, less than, greater than, equal to
 
 num = 0
 val = 0
 
-# FV(7) , FL(8) , FG(9) , FE(10) is flag register for overflow, less than, greater than, equal to
-
-def dec_2_bin(num:float)->str:
-    a=str(num)
-    before=""
-    after=""
-    flag=True
-    for i in a:
-        if(i=='.'):
-            flag=False
-        if(flag):
-            before+=i
-        else:
-            after+=i
-    if(flag):
-        return "0"
-    before=str(bin(int(before))[2::])
-    after=float(after)
-    temp=""
-    while(after!=0):
-        after*=2
-        temp+=str(int(after))
-        after=after-int(after)
-    if(len(before)>8 or before=="0" or len(before)-1+len(temp)>5):
-        return "0"
-    ans=""
-    x=len(bin(len(before)-1)[2::])
-    for i in range(3-x):
-        ans+="0"
-    ans+=bin(len(before)-1)[2::]+before[1::]+temp
-    for i in range(8-len(ans)):
-        ans+="0"
-    return ans
-
-def bin_2_dec(num:str)->float:
-    exp=int(num[:3:],2)
-    ans="1"
-    for i in range(exp):
-        if(3+i<8):
-            ans+=num[i+3]
-        else:
-            ans+="0"
-    ans=int(ans,2)
-    j=-1
-    for i in range(exp+3,8):
-        ans+=int(num[i])*math.pow(2,j)
-        j-=1
-    return ans
+def cust_bin(inpt:int, mylen:int)->str:
+    bin_val:str = bin(inpt)[2::]
+    bin_val = (mylen - len(bin_val))*'0' + bin_val
+    return bin_val
 
 def reset():
     register[7] = 0
@@ -81,13 +35,18 @@ def typeA(op:str, r1:str, r2:str, r3:str):
     val2 = int(r2, 2)
     val3 = int(r3, 2)
 
+    global register
+
+    # print(f"{register[val1]} : {val1}")
+    # print(f"{register[val2]} : {val2}")
+    # print(f"{register[val3]} : {val3}")
     #Add
     if(op == "10000"):
         reset()
         register[val3] = register[val2] + register[val1]
 
         if(register[val3] >= 65536):
-            flags["overflow"] = True
+            register[7] |= (1 << 3)
             register[val3] %= 65536
         
     #Subtract
@@ -97,7 +56,7 @@ def typeA(op:str, r1:str, r2:str, r3:str):
             register[val3] = register[val2] - register[val1]
         else:
             register[val3] = 0  
-            flags["overflow"] = True
+            register[7] |= (1 << 3)
         
     #Multiply
     elif(op == "10110"):
@@ -105,7 +64,7 @@ def typeA(op:str, r1:str, r2:str, r3:str):
         register[val3] = register[val2] * register[val1]
 
         if(register[val3] >= 65536):
-            flags["overflow"] = True
+            register[7] |= (1 << 3)
 
     #XOR
     elif(op == "11010"):
@@ -179,14 +138,14 @@ def typeC(op:str, r1:str, r2:str):
 
     #Compare
     elif(op == "11110"):
-        if(val1 > val2):
-            num |= (1 << 1)
+        if(register[val1] > register[val2]):
+            register[7] |= (1 << 1)
         
-        elif(val1 < val2):
-            num |= (1 << 2)
+        elif(register[val1] < register[val2]):
+            register[7] |= (1 << 2)
         
-        elif(val1 == val2):
-            num |= (1 << 0) 
+        elif(register[val1] == register[val2]):
+            register[7] |= (1 << 0) 
 
 def typeD(op:str, r1:str, mem_addr:str):
     
@@ -207,31 +166,35 @@ def typeD(op:str, r1:str, mem_addr:str):
 
 def typeE(op:str, mem_addr:str):
     
-    global pc
+    global new_pc
     val_mem = int(mem_addr, 2)
 
     #unconditional jump
     if(op == "11111"):
-        pc = val_mem
+        new_pc = val_mem -1
+        reset()
 
     #jump if less
     elif(op == "01100"):
         if(register[7] == 4 or register[7] == 12):
-            pc = val_mem
+            new_pc = val_mem -1
+        reset()
 
     #jump if greater
     elif(op == "01101"):
         if(register[7] == 2 or register[7] == 10):
-            pc = val_mem
+            new_pc = val_mem -1
+        reset()
 
     #jump if equal
     elif(op == "01111"):
         if(register[7] == 1 or register[7] == 9):
-            pc = val_mem
-    
-    #return pc
+            new_pc = val_mem -1
+        reset()
 
 def load_memory():
+    # sys.stdin = open('input.txt', 'r')
+    # sys.stdout = open('output.txt', 'w')
     for i in range(256):
         try:
             inpt = input()
@@ -245,12 +208,12 @@ def load_memory():
     return
 
 def process_inst(inst:int):
-    k = str(inst)
+    k = cust_bin(inst, 16)
     op = k[0:5]
     mytype = dictionary[op]
     global halted
     if(mytype == 'A'):
-        typeA(op, k[7:10], k[10:13], k[13:16])
+        typeA(op, k[10:13], k[13:16], k[7:10])
 
     elif(mytype == 'B'):
         typeB(op, k[5:8], k[8:16])
@@ -258,6 +221,8 @@ def process_inst(inst:int):
     elif(mytype == 'C'):
         typeC(op, k[10:13], k[13:16])
     
+    #11110 00000 010 011
+
     elif(mytype == 'D'):
         typeD(op, k[5:8], k[8:16])
 
@@ -268,15 +233,15 @@ def process_inst(inst:int):
         halted = True
 
 def out_state(pc:int):
-    print(pc, end = ' ')
+    print(cust_bin(pc, 8), end = ' ')
     for reg in register:
-        print(bin(reg)[2::], end = ' ')
+        print(cust_bin(reg, 16), end = ' ')
     print()
     return
 
 def out_mem():
     for line in mem:
-        print(bin(line)[2::])
+        print(cust_bin(line, 16))
     return
 
 def update_cycledata(pc:int):  #For q4
@@ -290,13 +255,24 @@ def plot_scatter():
     return
 
 def main():
+    # sys.stdout = open('output.txt', 'w')
     load_memory()
-    while(not halted):
+    #global halted
+    cnt = 0
+    while((not halted) and (cnt < 150)):
+        global pc
+        global new_pc
         pc += 1
         inst = mem[pc]
         process_inst(inst)
         out_state(pc)
         update_cycledata(pc)
+        cnt += 1
+
+        if (new_pc != -1):
+            pc = new_pc
+            new_pc = -1
+
     out_mem()
     plot_scatter()
     return
